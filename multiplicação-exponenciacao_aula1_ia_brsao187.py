4885df0e1d99158136ad717a4168364a5796696a
rodoxbianchi <bianchirodolfo99@gmail.com>
2025-07-07 10:50:31 -0300

    arquivo deletado

--- multiplicação-exponenciacao_aula1_ia_brsao187.py
+++
@@ -1,7 +0,0 @@
-#Dados da caixa são: comprimento=12,largura=20,altura=40
-comprimento = int(input("Digite o comprimento do objeto:"))
-largura = int(input("Digite o comprimento do objeto:"))
-altura = int(input("Digite o comprimento do objeto:"))
-
-volume = comprimento * largura * altura
-print (f"O volume da caixa é: {volume} cm³")
