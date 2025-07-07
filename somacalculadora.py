66cbea649a6c5a2b3e162bff4632bdf4f727562b
rodoxbianchi <bianchirodolfo99@gmail.com>
2025-07-07 10:50:51 -0300

    arquivo deletado

--- somacalculadora.py
+++
@@ -1,4 +0,0 @@
-numero1 = int(input ("Digite um numero inteiro: "))
-numero2 = int(input ("Digite um numero inteiro: "))
-soma = numero1 + numero2
-print("A soma dos dois numeros inteiros é:", soma)
